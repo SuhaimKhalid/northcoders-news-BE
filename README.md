760d6c0479417b0b9f1bd70243882fdffd7a90b9
Suhaim <suhaimkhalid007@gmail.com>
2025-04-15 17:08:06 +0100

    Complete with insert of data in test file with complete pass of all seed.test file test

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+# How To Add ENV File And Connect to DataBase
+
+- First you have to create to .env files with the names of:
+- 1 .env.test (For Test)
+- 2 .env.development (For Development)
+
+  For each of file you have to connect it to its relative DataBase
+  To Do So you have to write
+
+- 1 PGDATABASE = nc_news(NAME OF YOUR DATABASE )
+- 2 PGDATABASE = nc_news_test(NAME OF YOUR Test DATABASE )
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,110 @@
-const db = require("../connection")
-
+const db = require("../connection");
+const format = require("pg-format");
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(`DROP TABLE IF EXISTS comments`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics`);
+    }) /** Creation of Table start from here */
+    .then(() => {
+      return db.query(`
+  CREATE TABLE topics(
+  slug VARCHAR(299) PRIMARY KEY,
+  description VARCHAR(300) NOT NULL,
+  img_url VARCHAR(1000));
+  `);
+    })
+    .then(() => {
+      return db.query(`
+          CREATE TABLE users (
+          username VARCHAR(500) PRIMARY KEY,
+          name VARCHAR(200) NOT NULL,
+          avatar_url VARCHAR(1000)
+        );`);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE articles (
+          article_id SERIAL PRIMARY KEY,
+          title VARCHAR(500) NOT NULL,
+          topic VARCHAR(1000) REFERENCES topics(slug),
+          author VARCHAR(500) REFERENCES users(username),
+          body TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          votes INT DEFAULT 0,
+          article_img_url VARCHAR(1000)
+        );
+        `);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments(
+          comment_id SERIAL PRIMARY KEY,
+          article_id INT REFERENCES articles(article_id),
+          body TEXT NOT NULL,
+          votes INT DEFAULT 0,
+          author VARCHAR(500) REFERENCES users(username),
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);`);
+    }) /** insertaion of Table start from here */
+    .then(() => {
+      const formatedTableTopic = topicData.map((obj) => {
+        return [obj.slug, obj.description, obj.img_url];
+      });
+      const insertTopicTable = format(
+        `INSERT INTO topics (slug, description,img_url) VALUES %L`,
+        formatedTableTopic
+      );
+      return db.query(insertTopicTable);
+    })
+    .then(() => {
+      const formatedUserTable = userData.map((obj) => {
+        return [obj.username, obj.name, obj.avatar_url];
+      });
+      const inseretedUser = format(
+        `INSERT INTO users (username, name, avatar_url) VALUES %L`,
+        formatedUserTable
+      );
+      return db.query(inseretedUser);
+    })
+    .then(() => {
+      const formatedArticelTale = articleData.map((obj) => {
+        return [
+          obj.title,
+          obj.topic,
+          obj.author,
+          obj.body,
+          new Date(obj.created_at).toISOString(),
+          obj.votes,
+          obj.article_img_url,
+        ];
+      });
+      const insertArticleData = format(
+        `
+        INSERT INTO articles (title,topic,author,body,created_at,votes,article_img_url) VALUES %L`,
+        formatedArticelTale
+      );
+      return db.query(insertArticleData);
+    })
+    .then(() => {
+      const formatedCommentsTable = commentData.map((obj) => {
+        return [
+          obj.body,
+          obj.votes,
+          obj.author,
+          new Date(obj.created_at).toISOString(),
+        ];
+      });
+      const InsertedCommentsTable = format(
+        `INSERT INTO comments (body,votes,author,created_at) VALUES %L`,
+        formatedCommentsTable
+      );
+
+      return db.query(InsertedCommentsTable);
+    });
 };
 module.exports = seed;
