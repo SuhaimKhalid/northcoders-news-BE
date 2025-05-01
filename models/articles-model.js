8eb2c75154ed7f47d0a727e85f5efdf5f686ccfd
Suhaim <suhaimkhalid007@gmail.com>
2025-05-01 11:54:05 +0100

    Complete task 8

--- models/articles-model.js
+++ models/articles-model.js
@@ -59,7 +59,7 @@ const updateVotesByArticleId = (article_id, inc_votes) => {
 
   return db
     .query(
-      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *",
+      "UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *;",
       [inc_votes, article_id]
     )
     .then((result) => {
