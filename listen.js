2c03f99868c85da74cb8c87a309b0da0accc78d7
Suhaim <suhaimkhalid007@gmail.com>
2025-05-02 15:21:17 +0100

    setup the project to host

--- listen.js
+++ listen.js
@@ -1,9 +1,13 @@
 const app = require("./api");
 
-app.listen(9090, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Sever is Running!");
-  }
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+// app.listen(9090, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Sever is Running!");
+//   }
+// });
