631f0a28f32fc2c8e66ce0e5075c0b96f359d93e
Suhaim <suhaimkhalid007@gmail.com>
2025-05-19 11:19:39 +0100

    Adjust Topic Query

--- models/articles-model.js
+++ models/articles-model.js
@@ -32,7 +32,7 @@ const selectAllArticles = (article_id, sort_by, order, topic) => {
 };
 function queriescondition(queryStr, sort_by, order, topic) {
   // For to add conditon in query for topics
-  const topicGreenListing = ["mitch", "cats", "cooking", "Cooding", "Football"];
+  const topicGreenListing = ["mitch", "cats", "cooking", "coding", "football"];
   if (topic && topicGreenListing.includes(topic.toLowerCase())) {
     queryStr += ` WHERE topic = '${topic.toLowerCase()}';`;
   }
